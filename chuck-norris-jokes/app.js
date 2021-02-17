915a2a1a99fea21df47677718121c683c81843cb
Jerry Pageot <jerrypageot@gmail.com>
2021-02-17 12:35:47 -0500

    added new animation with js

--- chuck-norris-jokes/app.js
+++ chuck-norris-jokes/app.js
@@ -1,6 +1,7 @@
 
 const btn = document.querySelector('.btn');
 const content = document.querySelector('.content');
+const img = document.querySelector('.container img');
 
 const URL = 'https://api.chucknorris.io/jokes/random';
 
@@ -18,8 +19,16 @@ function getData(url){
         if(xhr.readyState !== 4) return;
 
         if(xhr.status === 200){
+            img.classList.add('shake-img')
             const {value:joke} = JSON.parse(xhr.responseText);
-            content.textContent = joke
+            content.textContent = joke;
+
+            const random =Math.random() * 1000;
+
+            setTimeout(() => {
+                img.classList.remove('shake-img')
+            }, random);
+            
         }else{
             console.log({
                 status: xhr.status,
