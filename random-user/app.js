8ee2cd42237491c258c9a246c8d780c90e55fce4
Jerry Pageot <jerrypageot@gmail.com>
2021-02-20 14:27:38 -0500

    added display user js functitonality to project

--- random-user/app.js
+++ random-user/app.js
@@ -1,44 +1,21 @@
 
-import getElement from './utils/getElement.js'
+import getElement from './utils/getElement.js';
+import getUser from './utils/fetchUser.js';
+import displayUser from './utils/displayUser.js'
 
 const URL = 'https://randomuser.me/api/';
 
-const img = getElement('.user-img');
-const title = getElement('.user-title');
-const value = getElement('.user-value');
 const btn = getElement('.btn');
 
-const btns = [...document.querySelectorAll('.icon')];
-
-const getUser = async () => {
-    const response = await fetch(URL);
-    const data = await response.json();
-    //data destructure
-    const person = data.results[0];
-    const {phone,email} = person;
-    const {large:image} = person.picture;
-    const {password} = person.login;
-    const {first,last} = person.name;
-    const {dob:{age}} = person; //different way but same reuslt
-    const {street:{number, name}} = person.location;
-
-    return{
-        phone,
-        email,
-        image,
-        password,
-        age,
-        name: `${first} ${last}`,
-        street: `${number} ${name}`,
-
-    }
-    console.log(person);
-}
-
-const showUser = () => {
+const showUser = async () => {
     //get user from api
-    getUser().then(data => console.log(data));
+    const person = await getUser();
+
+    //display user
+    displayUser(person)
 }
-window.addEventListener('DOMContentLoaded', showUser)
-btn.addEventListener('click', showUser)
+
+
+window.addEventListener('DOMContentLoaded', showUser);
+btn.addEventListener('click', showUser);
 
